caf39f25142cc88bf949e472a5c9cc862f84ebc3
aytiqaqash <aytiqaqash@gmail.com>
2023-08-31 16:43:45 -0400

    Add CountryService methods
    Description:
    methods are insert, update, findById, findAll.

--- src/main/java/com/webperside/courseerpbackend/services/country/CountryServiceImpl.java
+++ src/main/java/com/webperside/courseerpbackend/services/country/CountryServiceImpl.java
@@ -1,10 +1,13 @@
 package com.webperside.courseerpbackend.services.country;
 
+import com.webperside.courseerpbackend.exception.BaseException;
 import com.webperside.courseerpbackend.models.mybatis.country.Country;
 import com.webperside.courseerpbackend.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -17,4 +20,23 @@ public class CountryServiceImpl implements CountryService{
     public void insert(Country country) {
         countryRepository.insert(country);
     }
+
+    @Override
+    public Country findById(long id) {
+        return countryRepository.findById(id).orElseThrow(
+                () -> BaseException.notFound(Country.class.getSimpleName(),"country",String.valueOf(id))
+        );
+    }
+
+    @Override
+    public List<Country> findAll() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public void update(Country country) {
+        countryRepository.update(country);
+    }
+
+
 }
