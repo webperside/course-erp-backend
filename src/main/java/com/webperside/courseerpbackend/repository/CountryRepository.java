1b66659f6e56179e70d4d97659cc86c967e703c9
KhadijaGadirova <khadijagadirova1@gmail.com>
2023-09-19 14:55:56 +0400

    created crud services for app configs table

--- src/main/java/com/webperside/courseerpbackend/repository/CountryRepository.java
+++ src/main/java/com/webperside/courseerpbackend/repository/CountryRepository.java
@@ -3,7 +3,6 @@ package com.webperside.courseerpbackend.repository;
 import com.webperside.courseerpbackend.models.mybatis.country.Country;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
