c9217741c1ab70ab561fb97c143f1c13e07ec545
aytiqaqash <aytiqaqash@gmail.com>
2023-09-20 20:22:37 -0400

    Update return to BaseResponse.success
    Description:
    Probably we will change success to created later.

--- src/main/java/com/webperside/courseerpbackend/controller/CountryController.java
+++ src/main/java/com/webperside/courseerpbackend/controller/CountryController.java
@@ -1,10 +1,12 @@
 package com.webperside.courseerpbackend.controller;
 
+import com.webperside.courseerpbackend.models.base.BaseResponse;
 import com.webperside.courseerpbackend.models.mappers.CountryEntityMapper;
 import com.webperside.courseerpbackend.models.mybatis.country.Country;
 import com.webperside.courseerpbackend.models.payload.country.CountryPayload;
 import com.webperside.courseerpbackend.services.country.CountryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,16 +18,18 @@ public class CountryController {
     private static final CountryEntityMapper countryEntityMapper = CountryEntityMapper.INSTANCE;
 
     @PostMapping("/country")
-    public void insert(@RequestBody CountryPayload countryPayload){
+    public BaseResponse<Void> insert(@RequestBody CountryPayload countryPayload){
         Country country = countryEntityMapper.toEntity(countryPayload);
         countryService.insert(country);
+        return BaseResponse.success();
     }
 
     @PutMapping("/country/{id}")
-    public void update(@PathVariable("id") long id, @RequestBody CountryPayload countryPayload){
+    public BaseResponse<Void> update(@PathVariable("id") long id, @RequestBody CountryPayload countryPayload){
         countryPayload.setId(id);
         Country country = countryEntityMapper.toEntity(countryPayload, new Country());
         countryService.update(country);
+        return BaseResponse.success();
     }
 
 }
