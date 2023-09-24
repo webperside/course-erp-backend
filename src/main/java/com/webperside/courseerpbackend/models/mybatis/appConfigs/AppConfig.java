51db9d4437a0e035009ba5fe1b19b6c8acd3c404
KhadijaGadirova <khadijagadirova1@gmail.com>
2023-09-24 23:59:47 +0400

    Builder changed to Super builder in AppConfig

--- src/main/java/com/webperside/courseerpbackend/models/mybatis/appConfigs/AppConfig.java
+++ src/main/java/com/webperside/courseerpbackend/models/mybatis/appConfigs/AppConfig.java
@@ -3,16 +3,10 @@ package com.webperside.courseerpbackend.models.mybatis.appConfigs;
 import com.webperside.courseerpbackend.models.mybatis.base.BaseEntity;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-<<<<<<< HEAD
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
-=======
-
-@Data
-@Builder
->>>>>>> aff6fe5f3a790ea037fb3232dce391596a513f69
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
