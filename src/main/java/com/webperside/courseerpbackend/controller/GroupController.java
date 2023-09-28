91c6f26116cbae1b61c5ceb51232d4b4d7ea804e
Tahir <128832793+ttayping@users.noreply.github.com>
2023-09-28 15:25:28 +0400

    update mapper instance in Group Controller

--- src/main/java/com/webperside/courseerpbackend/controller/GroupController.java
+++ src/main/java/com/webperside/courseerpbackend/controller/GroupController.java
@@ -13,19 +13,18 @@ import org.springframework.web.bind.annotation.*;
 public class GroupController {
 
     private final GroupService groupService;
-    private static final GroupEntityMapper groupEntityMapper =GroupEntityMapper.INSTANCE;
 
     // todo validation
     @PostMapping
     public BaseResponse<Void> addGroup (@RequestBody GroupPayload groupPayload){
-        groupService.insert(groupEntityMapper.toEntity(groupPayload));
+        groupService.insert(GroupEntityMapper.INSTANCE.toEntity(groupPayload));
         return BaseResponse.success();
     }
 
     // todo validation
     @PutMapping("/{id}")
     public BaseResponse<Void> updateGroup (@PathVariable("id") long id, @RequestBody GroupPayload groupPayload) {
-        groupService.update(groupEntityMapper.toEntity(groupPayload, id));
+        groupService.update(GroupEntityMapper.INSTANCE.toEntity(groupPayload, id));
         return BaseResponse.success();
     }
 }
