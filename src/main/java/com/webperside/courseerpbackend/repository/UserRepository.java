028c0b965c2321500411b39241b212119302d88c
Tahir <128832793+ttayping@users.noreply.github.com>
2023-09-28 12:10:23 +0400

    delete unused import

--- src/main/java/com/webperside/courseerpbackend/repository/UserRepository.java
+++ src/main/java/com/webperside/courseerpbackend/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.webperside.courseerpbackend.repository;
 import com.webperside.courseerpbackend.models.mybatis.user.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
